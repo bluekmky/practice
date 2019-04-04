e2080b3d4e24bd084af79bec3f057bc49681fec7
bluekmky <bluekmky@naver.com>
2019-04-04 19:49:00 +0900

    homework

---
+++ test.py
@@ -0,0 +1,39 @@
+# 첫번째 실습
+# a = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# c = 0
+# c = c + 1 
+# for b in a:
+    # if b > 5:
+        # print("%d 이 수는 5보다 큽니다." %c)
+    # else:
+        # if b == 5:
+            # print("%d 이수는 5입니다." %c) 
+        # else:    
+            # print("%d 이 수는 5보다 작습니다." %c)
+
+# 두번쨰 실습
+# dic = {"a" : 12, "b" : 11, "c": 13 }
+
+# v = 0
+
+# for k in dic:
+    # v = v + 1   
+    # print("key %d:" %v + k)
+    # print("value %d:" %v + str(dic[k]))
+    # print('-'*20)
+
+
+# 세번째 실습
+# a="1 2 3 4 5 6 7 8 9 0"
+# b = a.split(" ")
+# print(b)
+
+# 과제
+a="마재훈은 멋쟁이 사자처럼 7기 사자이다."
+b = a.split(" ")
+for r in b:
+    print(r)
+
+
+
